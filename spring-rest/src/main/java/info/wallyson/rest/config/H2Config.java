e82d3d116f006d76059a06bacda86ed7732f8d0c
wallyson <wallyson@leanonsystems.com>
2021-04-15 08:50:15 -0300

    fixed issue with accessing H2 outside container

---
+++ spring-rest/src/main/java/info/wallyson/rest/config/H2Config.java
@@ -0,0 +1,14 @@
+package info.wallyson.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2Config {
+    @Bean(initMethod="start", destroyMethod="stop")
+    public org.h2.tools.Server h2WebConsoleServer () throws SQLException {
+        return org.h2.tools.Server.createWebServer("-web","-webAllowOthers","-webDaemon","-webPort", "8091");
+    }
+}
